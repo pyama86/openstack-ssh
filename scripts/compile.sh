4967cf55244c2fd81b1c525c551a61f61f63be01
pyama86 <pyama@pepabo.com>
2015-08-09 16:10:38 +0900

    add compile scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,7 +4,7 @@ pkg_path="$GOPATH/src/github.com/pyama86/openstack-ssh/pkg"
 XC_ARCH=${XC_ARCH:-386 amd64}
 XC_OS=${XC_OS:-linux}
 cd "$GOPATH/src/github.com/pyama86/openstack-ssh/cmd/openstack-ssh"
-rm -rf pkg/
+rm -rf $pkg_path
 gox \
     -os="${XC_OS}" \
     -arch="${XC_ARCH}" \
